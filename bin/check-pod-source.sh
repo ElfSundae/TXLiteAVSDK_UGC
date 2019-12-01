81c381caaf3e3621139b063cbac23580319db944
Elf Sundae <elf.sundae@gmail.com>
2019-12-01 17:27:29 +0800

    Update script

--- bin/check-pod-source.sh
+++ bin/check-pod-source.sh
@@ -1,42 +1,32 @@
-#!/bin/sh
+#!/bin/bash
+
 #
 # Check TXLiteAVSDK_UGC pod source
 #
-
-# $ pod spec which TXLiteAVSDK_UGC
-# ~/.cocoapods/repos/trunk/Specs/a/3/e/TXLiteAVSDK_UGC/6.7.7754/TXLiteAVSDK_UGC.podspec.json
-
-## Existing version from CocoaPods Specs:
-# 4.2.3427  Free
-# 4.4.3774  Free
-# 4.5.4018  Licensed
-
 # Result:
 # https://liteavsdk-1252463788.cosgz.myqcloud.com/4.2/TXLiteAVSDK_UGC_Rename_iOS_4.2.3427.zip
 # https://liteavsdk-1252463788.cosgz.myqcloud.com/4.3/TXLiteAVSDK_UGC_Rename_iOS_4.3.3609.zip
 # https://liteavsdk-1252463788.cosgz.myqcloud.com/4.4/TXLiteAVSDK_UGC_Rename_iOS_4.4.3774.zip
 # https://liteavsdk-1252463788.cosgz.myqcloud.com/4.5/TXLiteAVSDK_UGC_Rename_iOS_4.5.4018.zip
 
-
-# Usage: _url 4.2 3774
-_url()
-{
-    echo "https://liteavsdk-1252463788.cosgz.myqcloud.com/$1/TXLiteAVSDK_UGC_Rename_iOS_$1.$2.zip"
-}
+## Known versions from CocoaPods Specs: https://github.com/CocoaPods/Specs/tree/master/Specs/a/3/e/TXLiteAVSDK_UGC
+# 4.2.3427  Free
+# 4.4.3774  Free
+# 4.5.4018  Licensed
 
 # Usage: _check 4.2 3774
 _check()
 {
-    url=`_url $@`
+    url="https://liteavsdk-1252463788.cosgz.myqcloud.com/$1/TXLiteAVSDK_UGC_Rename_iOS_$1.$2.zip"
     # https://superuser.com/a/442395/227501
-    code=`curl -s -o /dev/null -I -w "%{http_code}" "$url"`
+    httpCode=`curl -s -o /dev/null -I -w "%{http_code}" "$url"`
 
     if [[ $? != 0 ]]; then
         echo "*** Error ***"
         exit 1
     fi
 
-    if [[ $code == 200 ]]; then
+    if [[ $httpCode == 200 ]]; then
         echo "$url"
     fi
 }
